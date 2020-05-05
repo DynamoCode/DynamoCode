d8c4ed2c374aeab5d1acd114df3f2988f61e2d04
Nestor Collazo <nestor.collazo@hotmail.com>
2020-05-05 08:37:48 -0400

    Updated NHibernate UnitOfWork

--- src/DynamoCode.Infrastructure.Data.NHibernate/UoW/UnitOfWork.cs
+++ src/DynamoCode.Infrastructure.Data.NHibernate/UoW/UnitOfWork.cs
@@ -2,8 +2,10 @@ using System;
 using System.Threading.Tasks;
 using NHibernate;
 
-namespace DynamoCode.Infrastructure.Data.NHibernate.UoW {
-    public class UnitOfWork : INHUnitOfWork {
+namespace DynamoCode.Infrastructure.Data.NHibernate.UoW
+{
+    public class UnitOfWork : INHUnitOfWork
+    {
         protected ISessionFactory _sessionFactory;
 
         protected ISession _session;
@@ -11,79 +13,111 @@ namespace DynamoCode.Infrastructure.Data.NHibernate.UoW {
 
         protected bool _autoCommit = false;
 
-        public UnitOfWork (ISessionFactory sessionFactory) {
+        public UnitOfWork(ISessionFactory sessionFactory)
+        {
             _sessionFactory = sessionFactory;
         }
 
-        public UnitOfWork (ISessionFactory sessionFactory, bool autoCommit) {
-            _sessionFactory = sessionFactory;
-            _autoCommit = autoCommit;
-        }
-
-        public virtual ISession Session {
-            get {
-                if (_session == null) {
-                    _session = _sessionFactory.OpenSession ();
+        public virtual ISession Session
+        {
+            get
+            {
+                if (_session == null)
+                {
+                    _session = _sessionFactory.OpenSession();
                 }
-                BeginTransaction ();
+                BeginTransaction();
                 return _session;
             }
         }
 
-        public void BeginTransaction () {
-            if (_transaction == null) {
-                _transaction = _session.BeginTransaction ();
+        public void BeginTransaction()
+        {
+            if (_transaction == null)
+            {
+                _transaction = _session.BeginTransaction();
             }
         }
 
-        public virtual void Commit () {
-            try {
-                if (_transaction != null && _transaction.IsActive) {
-                    _transaction.Commit ();
+        private void CommitTransaction()
+        {
+            try
+            {
+                if (_transaction != null && _transaction.IsActive)
+                {
+                    _transaction.Commit();
                 }
-            } catch (Exception e) {
-                _transaction.Rollback ();
-                throw e;
-            } finally {
-                if (_transaction != null) {
-                    _transaction.Dispose ();
+            }
+            catch (Exception)
+            {
+                _transaction.Rollback();
+            }
+            finally
+            {
+                if (_transaction != null)
+                {
+                    _transaction.Dispose();
                     _transaction = null;
                 }
             }
         }
 
-        public virtual void Rollback () {
-            if (_transaction != null && _transaction.IsActive) {
-                _transaction.Rollback ();
-                _transaction.Dispose ();
-                _transaction = null;
-            }
-            if (_transaction != null) {
-                _transaction.Dispose ();
-                _transaction = null;
-            }
-        }
-
-        public virtual void Dispose () {
-            if (_session != null) {
-                try {
-                    if (_autoCommit) {
-                        Commit ();
-                    } else {
-                        Rollback ();
-                    }
-                } catch (Exception e) {
-                    throw e;
-                } finally {
-                    _session.Close ();
-                    _session.Dispose ();
+        public virtual void Commit()
+        {
+            if (_session != null)
+            {
+                try
+                {
+                    CommitTransaction();
+                }
+                finally
+                {
+                    _session.Close();
+                    _session.Dispose();
                     _session = null;
                 }
             }
         }
 
-        public Task CommitAsync () {
-            return _transaction.CommitAsync ();
+        public virtual void Rollback()
+        {
+            if (_transaction != null && _transaction.IsActive)
+            {
+                _transaction.Rollback();
+                _transaction.Dispose();
+                _transaction = null;
+            }
+            if (_transaction != null)
+            {
+                _transaction.Dispose();
+                _transaction = null;
+            }
+        }
+
+        public virtual void Dispose()
+        {
+            if (_session != null)
+            {
+                try
+                {
+                   CommitTransaction();
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+                finally
+                {
+                    _session.Close();
+                    _session.Dispose();
+                    _session = null;
+                }
+            }
+        }
+
+        public Task CommitAsync()
+        {
+            return _transaction.CommitAsync();
         }
     }
 }
