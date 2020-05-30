7c5f1694552514b9d8bb0c2aa92d22d6a4543d9b
Nestor Collazo <nestor.collazo@hotmail.com>
2020-05-30 10:16:54 -0400

    Updated UnitOfWork

--- src/DynamoCode.Infrastructure.Data.EntityFramework/UoW/UnitOfWork.cs
+++ src/DynamoCode.Infrastructure.Data.EntityFramework/UoW/UnitOfWork.cs
@@ -1,8 +1,8 @@
 using System.Threading.Tasks;
-using DynamoCode.Infrastructure.Data.EntityFramework.UoW;
 using Microsoft.EntityFrameworkCore;
 
-namespace DynamoCode.Infrastructure.Data.EntityFramework.UoW {
+namespace DynamoCode.Infrastructure.Data.EntityFramework.UoW
+{
     public class UnitOfWork : DbContext, IEFUnitOfWork {
         public UnitOfWork (DbContextOptions<UnitOfWork> options) : base (options) { }
 
--- src/DynamoCode.Infrastructure.Data.NHibernate/Repositories/ReadRepository.cs
+++ src/DynamoCode.Infrastructure.Data.NHibernate/Repositories/ReadRepository.cs
@@ -3,32 +3,40 @@ using System.Linq;
 using DynamoCode.Domain.Data.Interfaces;
 using DynamoCode.Infrastructure.Data.NHibernate.UoW;
 
-namespace DynamoCode.Infrastructure.Data.NHibernate.Repositories {
-    public class ReadRepository<TKey, T> : IReadRepository<TKey, T> where T : class {
+namespace DynamoCode.Infrastructure.Data.NHibernate.Repositories
+{
+    public class ReadRepository<TKey, T> : IReadRepository<TKey, T> where T : class
+    {
         protected readonly INHUnitOfWork _unitOfWork;
 
-        public ReadRepository (INHUnitOfWork unitOfWork) {
+        public ReadRepository(INHUnitOfWork unitOfWork)
+        {
             _unitOfWork = unitOfWork;
         }
 
-        public virtual T FindBy (TKey id) {
-            return _unitOfWork.Session.Get<T> (id);
+        public virtual T FindBy(TKey id)
+        {
+            return _unitOfWork.Session.Get<T>(id);
         }
 
-        public virtual IList<T> All () {
-            return _unitOfWork.Session.Query<T> ().ToList ();
+        public virtual IList<T> All()
+        {
+            return _unitOfWork.Session.Query<T>().ToList();
         }
 
-        public IList<T> All (int page, int itemsPerPage) {
-            return _unitOfWork.Session.Query<T> ().ToPage (page, itemsPerPage).ToList ();
+        public IList<T> All(int page, int itemsPerPage)
+        {
+            return _unitOfWork.Session.Query<T>().ToPage(page, itemsPerPage).ToList();
         }
 
-        public int Count () {
-            return _unitOfWork.Session.Query<T> ().Count ();
+        public int Count()
+        {
+            return _unitOfWork.Session.Query<T>().Count();
         }
     }
 
-    public class ReadRepository<T> : ReadRepository<int, T>, IReadRepository<T> where T : class {
-        public ReadRepository (INHUnitOfWork unitOfWork) : base (unitOfWork) { }
+    public class ReadRepository<T> : ReadRepository<int, T>, IReadRepository<T> where T : class
+    {
+        public ReadRepository(INHUnitOfWork unitOfWork) : base(unitOfWork) { }
     }
 }
